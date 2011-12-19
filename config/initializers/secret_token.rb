4ba86c37389a7b7522df8733e887777422029b5e
Joe Savak <jsavak@gmail.com>
2011-12-19 21:09:47 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+APICompare::Application.config.secret_token = '3ae710e6f101c0af605f7753cccce3686bfbf6d9bad1437312885ffa0e197bff99ef09bed61f21a98fef53b33af4c9c2aaf484d70f369546147a948d73efdaa4'
