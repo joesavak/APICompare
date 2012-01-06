0f6734ebe7e7da8b1cde521d426018d91583e9a1
Joe Savak <jsavak@gmail.com>
2012-01-06 21:45:58 +0000

    parsing via nokogiri the resource method and path and storing that

--- app/models/wadl.rb
+++ app/models/wadl.rb
@@ -7,15 +7,29 @@ class Wadl < ActiveRecord::Base
   has_many :resources, :foreign_key => "wadl_id"
   
   def quick_load
-    print "HERE in quick load-----\n"
+    print "----HERE in quick load-----\n"
     print self.wadlfile.path
     print "\n"
-    doc = Nokogiri::XML(open(self.wadlfile.path))
-    print "about to loop. Doc:\n"    
-    print "\n"
-    doc.xpath("resources").each do |resource|
-      print "looping\n"
-      print resource['path']
+    
+    @doc = Nokogiri::XML(File.open(self.wadlfile.path))
+    print "----Opened doc----\n"
+    print "----Searching doc-----\n"
+    @doc.css("resource").each do |resource|
+      resource.css("resource").each do |res2|
+        res2.css("method").each do |meth2| 
+
+           
+          if res2['path'] != "{alias}"
+            print "resource path: " + resource['path'] + res2['path'] + "  method: " + meth2['href'] + "\n"
+            re = Resource.new
+            re.wadl_id = self.id
+            re.name = meth2['href']
+            re.base_url = resource['path'] + res2['path'] 
+            re.save!
+          end
+          
+        end 
+      end
     end
   end 
 end
